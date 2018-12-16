c1fa17b66c6389fbff8ad86c1856982ea9bf683b
vargus <felton.vargas@gmail.com>
2018-12-16 10:17:40 -0800

    update

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,25 +1,27 @@
 import React from 'react';
 
 const Spinner = ({
-  size = 'md',            // (sm|md)
+  size = 'md',            // (xsm|sm|md)
   color = 'primary',      // (primary|white)
-  display = 'centered'    // (centered|inline)
+  display = 'inline'    // (centered|inline)
 }) => {
 
   const classes = `
     Spinner
     Spinner--color-${color}
     Spinner--size-${size}
-    Spinner--display-${size}
+    Spinner--display-${display}
   `;
 
   return (
     <div className={classes}>
-      <svg className="Spinner__svg" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+      <svg className="Spinner__svg" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
         <circle className="Spinner__path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30"></circle>
       </svg>
     </div>
   )
+
+  
 }
 
 export default Spinner;
